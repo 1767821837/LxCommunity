922574e9b8dc693c08dcbfc90e709346a412f8c3
1767821837 <1767821837@qq.com>
2019-05-22 16:50:12 +0800

    tijiao

---
+++ src/main/java/com/lx/web/servlet/UserController.java
@@ -0,0 +1,14 @@
+package com.lx.web.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/user")
+    public @ResponseBody  String adduser(){
+        return "欢饮";
+    }
+}
