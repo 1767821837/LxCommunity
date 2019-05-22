37e1d56595ec0a34401951be2e28ae8273ac110e
1767821837 <1767821837@qq.com>
2019-05-22 19:18:45 +0800

    tijiao

--- src/main/webapp/js/include.js
+++ src/main/webapp/js/include.js
@@ -2,5 +2,8 @@ $(function () {
     $.get("/html/head.html",function (data) {
         $("#header").html(data);
     });
+    $.get("/html/footer.html",function (data) {
+        $("#footer").html(data);
+    });
 
 });
